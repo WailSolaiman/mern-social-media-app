cb0c8b3dad0148319a4c82216aed8c2e5bac1d4f
Wail Solaiman <waily2@hotmail.com>
2019-09-27 12:51:55 +0200

    production deployment

--- server/express.js
+++ server/express.js
@@ -9,12 +9,12 @@ import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 import postRoutes from './routes/post.routes'
 import Template from '../template'
-import devBundle from './devBundle'
+//import devBundle from './devBundle'
 
 const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
-devBundle.compile(app)
+//devBundle.compile(app)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
